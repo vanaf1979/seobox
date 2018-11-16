1705ccbf9ec8f475195dfbfe358401561b849879
Stephan <vanaf1979@gmail.com>
2018-11-16 15:42:26 +0100

    Cleanup and Laravel Mix

--- includes/class-seobox-i18n.php
+++ includes/class-seobox-i18n.php
@@ -1,39 +1,10 @@
 <?php
 
-/**
- * Define the internationalization functionality
- *
- * Loads and defines the internationalization files for this plugin
- * so that it is ready for translation.
- *
- * @link       https://vanaf1979.nl
- * @since      1.0.0
- *
- * @package    Seobox
- * @subpackage Seobox/includes
- */
+class Seobox_i18n
+{
 
-/**
- * Define the internationalization functionality.
- *
- * Loads and defines the internationalization files for this plugin
- * so that it is ready for translation.
- *
- * @since      1.0.0
- * @package    Seobox
- * @subpackage Seobox/includes
- * @author     Vanaf1979 <contact@vanaf1979.nl>
- */
-class Seobox_i18n {
-
-
-	/**
-	 * Load the plugin text domain for translation.
-	 *
-	 * @since    1.0.0
-	 */
-	public function load_plugin_textdomain() {
-
+	public function load_plugin_textdomain()
+	{
 		load_plugin_textdomain(
 			'seobox',
 			false,
@@ -41,7 +12,5 @@ class Seobox_i18n {
 		);
 
 	}
-
-
 
 }
--- includes/class-seobox.php
+++ includes/class-seobox.php
@@ -1,217 +1,94 @@
 <?php
 
-/**
- * The file that defines the core plugin class
- *
- * A class definition that includes attributes and functions used across both the
- * public-facing side of the site and the admin area.
- *
- * @link       https://vanaf1979.nl
- * @since      1.0.0
- *
- * @package    Seobox
- * @subpackage Seobox/includes
- */
-
-/**
- * The core plugin class.
- *
- * This is used to define internationalization, admin-specific hooks, and
- * public-facing site hooks.
- *
- * Also maintains the unique identifier of this plugin as well as the current
- * version of the plugin.
- *
- * @since      1.0.0
- * @package    Seobox
- * @subpackage Seobox/includes
- * @author     Vanaf1979 <contact@vanaf1979.nl>
- */
-class Seobox {
-
-	/**
-	 * The loader that's responsible for maintaining and registering all hooks that power
-	 * the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      Seobox_Loader    $loader    Maintains and registers all hooks for the plugin.
-	 */
+class Seobox
+{
 	protected $loader;
 
-	/**
-	 * The unique identifier of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
-	 */
 	protected $plugin_name;
 
-	/**
-	 * The current version of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $version    The current version of the plugin.
-	 */
 	protected $version;
 
-	/**
-	 * Define the core functionality of the plugin.
-	 *
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
-	 * the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function __construct() {
-		if ( defined( 'PLUGIN_NAME_VERSION' ) ) {
+
+	public function __construct()
+	{
+		if ( defined( 'PLUGIN_NAME_VERSION' ) )
+		{
 			$this->version = PLUGIN_NAME_VERSION;
-		} else {
-			$this->version = '1.0.0';
 		}
+		else
+		{
+			$this->version = '0.1.0';
+		}
+
 		$this->plugin_name = 'seobox';
 
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
-
 	}
 
-	/**
-	 * Load the required dependencies for this plugin.
-	 *
-	 * Include the following files that make up the plugin:
-	 *
-	 * - Seobox_Loader. Orchestrates the hooks of the plugin.
-	 * - Seobox_i18n. Defines internationalization functionality.
-	 * - Seobox_Admin. Defines all hooks for the admin area.
-	 * - Seobox_Public. Defines all hooks for the public side of the site.
-	 *
-	 * Create an instance of the loader which will be used to register the hooks
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function load_dependencies() {
 
-		/**
-		 * The class responsible for orchestrating the actions and filters of the
-		 * core plugin.
-		 */
+	private function load_dependencies()
+	{
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-seobox-loader.php';
 
-		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-seobox-i18n.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the admin area.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-seobox-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-seobox-public.php';
 
 		$this->loader = new Seobox_Loader();
-
 	}
 
-	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the Seobox_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function set_locale() {
 
+	private function set_locale()
+	{
 		$plugin_i18n = new Seobox_i18n();
 
 		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
-
 	}
 
-	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_admin_hooks() {
 
+	private function define_admin_hooks()
+	{
 		$plugin_admin = new Seobox_Admin( $this->get_plugin_name(), $this->get_version() );
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
 	}
 
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
 
+	private function define_public_hooks()
+	{
 		$plugin_public = new Seobox_Public( $this->get_plugin_name(), $this->get_version() );
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
 	}
 
-	/**
-	 * Run the loader to execute all of the hooks with WordPress.
-	 *
-	 * @since    1.0.0
-	 */
-	public function run() {
+
+	public function run()
+	{
 		$this->loader->run();
 	}
 
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The name of the plugin.
-	 */
-	public function get_plugin_name() {
+
+	public function get_plugin_name()
+	{
 		return $this->plugin_name;
 	}
 
-	/**
-	 * The reference to the class that orchestrates the hooks with the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    Seobox_Loader    Orchestrates the hooks of the plugin.
-	 */
-	public function get_loader() {
+
+	public function get_loader()
+	{
 		return $this->loader;
 	}
 
-	/**
-	 * Retrieve the version number of the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The version number of the plugin.
-	 */
-	public function get_version() {
+
+	public function get_version()
+	{
 		return $this->version;
 	}
 
