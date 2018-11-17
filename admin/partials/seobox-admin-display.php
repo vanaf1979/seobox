c828c6c1e42da3ee284596184c0e7c61a34f995d
Stephan <vanaf1979@gmail.com>
2018-11-17 22:13:27 +0100

    Added a info link

--- admin/partials/seobox-admin-display.php
+++ admin/partials/seobox-admin-display.php
@@ -1,5 +1,6 @@
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<!-- Media upload button: https://jeroensormani.com/how-to-include-the-wordpress-media-selector-in-your-plugin/ -->
 <!-- https://developer.wordpress.org/plugins/internationalization/how-to-internationalize-your-plugin/ -->
 
 <div class="seobox-metabox">
