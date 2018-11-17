b021c7334774fbf97a661cf076d4078a71fa5ba9
Stephan <vanaf1979@gmail.com>
2018-11-17 22:26:02 +0100

    Removed comments

--- admin/partials/seobox-admin-display.php
+++ admin/partials/seobox-admin-display.php
@@ -1,7 +1,5 @@
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
-<!-- Media upload button: https://jeroensormani.com/how-to-include-the-wordpress-media-selector-in-your-plugin/ -->
-<!-- https://developer.wordpress.org/plugins/internationalization/how-to-internationalize-your-plugin/ -->
 
 <div class="seobox-metabox">
 
