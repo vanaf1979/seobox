d9780bd2f1b221f68df117b00ddd16c9ee138394
Stephan <vanaf1979@gmail.com>
2019-02-10 14:48:13 +0100

    Added google setting slayout

--- settings/partials/seobox-settings-google-display.php
+++ settings/partials/seobox-settings-google-display.php
@@ -1,20 +1,338 @@
 
 
-<h4><?php _e( 'Brwoser title', 'seobox' ); ?></h4>
+<h3><?php _e( 'Google/Search engines', 'seobox' ); ?></h3>
 
-<table width="100%">
-    <tr>
-        <td width="80" valign="top">
-            <div class="form-wrapper">
-                <label>Active</label>
-                <input type="checkbox" name="" />
-            </div>
-        </td>
-        <td valign="top">
-            <div class="form-wrapper">
-                <label>Default value</label>
-                <input type="text" name="" />
-            </div>
-        </td>
-    </tr>
+<table width="100%" class="seobox-settings-section">
+    <thead>
+        <tr>
+            <td valign="top" width="170">
+                <h4>Browser title</h4>
+            </td>
+            <td valign="top">
+                <!-- <div class="form-wrapper active">
+                    <input type="checkbox" name="" />
+                    <label>Show in post edit screens?</label>
+                </div> -->
+            </td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Active:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="addition" value="noen"/>
+                    <label>Yes</label>
+                    <input type="radio" name="addition" value="sitename"/>
+                    <label>No</label>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Addition:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="addition" value="noen"/>
+                    <label>None</label>
+                    <input type="radio" name="addition" value="sitename"/>
+                    <label>Sitename</label>
+                    <input type="radio" name="addition" value="custom"/>
+                    <label>Custom</label>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Custom addition:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                <input type="text" name="custom_addition" placeholder="Browser title custom addition"/>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Position:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="position" value="prefix"/>
+                    <label>Prefix</label>
+                    <input type="radio" name="addition" value="suffix"/>
+                    <label>Suffix</label>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Default value:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="text" name="default_value" placeholder="Browser title default valeu"/>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Max lenght:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="text" name="max_lenght" placeholder="Browser title max lenght"/>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Max length overflow:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="Max_lenght_overflow" value="trim"/>
+                    <label>Trim</label>
+                    <input type="radio" name="addition" value="warn"/>
+                    <label>Warn</label>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+<table width="100%" class="seobox-settings-section">
+    <thead>
+        <tr>
+            <td valign="top" width="170">
+                <h4>Keywords</h4>
+            </td>
+            <td valign="top">
+                <!-- <div class="form-wrapper active">
+                    <input type="checkbox" name="" />
+                    <label>Show in post edit screens?</label>
+                </div> -->
+            </td>
+        </tr>
+    </thead>
+    <tbody>
+    <tr class="row">
+            <td valign="top">
+                <label class="title">Active:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="addition" value="noen"/>
+                    <label>Yes</label>
+                    <input type="radio" name="addition" value="sitename"/>
+                    <label>No</label>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Default value:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="text" name="default_value" placeholder="Browser title default valeu"/>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+<table width="100%" class="seobox-settings-section">
+    <thead>
+        <tr>
+            <td valign="top" width="170">
+                <h4>Description</h4>
+            </td>
+            <td valign="top">
+                <!-- <div class="form-wrapper active">
+                    <input type="checkbox" name="" />
+                    <label>Show in post edit screens?</label>
+                </div> -->
+            </td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Active:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="addition" value="noen"/>
+                    <label>Yes</label>
+                    <input type="radio" name="addition" value="sitename"/>
+                    <label>No</label>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Default value:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="addition" value="noen"/>
+                    <label>None</label>
+                    <input type="radio" name="addition" value="sitename"/>
+                    <label>Post excerpt</label>
+                    <input type="radio" name="addition" value="custom"/>
+                    <label>Custom</label>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Custom default value:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                <input type="text" name="custom_addition" placeholder="Browser title custom addition"/>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Max lenght:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="text" name="max_lenght" placeholder="Browser title max lenght"/>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Max length overflow:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="Max_lenght_overflow" value="trim"/>
+                    <label>Trim</label>
+                    <input type="radio" name="addition" value="warn"/>
+                    <label>Warn</label>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+<table width="100%" class="seobox-settings-section">
+    <thead>
+        <tr>
+            <td valign="top" width="170">
+                <h4>Robots</h4>
+            </td>
+            <td valign="top">
+                <!-- <div class="form-wrapper active">
+                    <input type="checkbox" name="" />
+                    <label>Show in post edit screens?</label>
+                </div> -->
+            </td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Active:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="addition" value="noen"/>
+                    <label>Yes</label>
+                    <input type="radio" name="addition" value="sitename"/>
+                    <label>No</label>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Default value:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="addition" value="noen"/>
+                    <label>Index / Follow</label>
+                    <input type="radio" name="addition" value="sitename"/>
+                    <label>Noindex / Follow</label>
+                    <input type="radio" name="addition" value="custom"/>
+                    <label>Noindex / Nofollow</label>
+                    <input type="radio" name="addition" value="custom"/>
+                    <label>index / Nofollow</label>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+<table width="100%" class="seobox-settings-section">
+    <thead>
+        <tr>
+            <td valign="top" width="170">
+                <h4>Cannonical link</h4>
+            </td>
+            <td valign="top">
+                <!-- <div class="form-wrapper active">
+                    <input type="checkbox" name="" />
+                    <label>Show in post edit screens?</label>
+                </div> -->
+            </td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Active:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="addition" value="noen"/>
+                    <label>Yes</label>
+                    <input type="radio" name="addition" value="sitename"/>
+                    <label>No</label>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Default value:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                    <input type="radio" name="addition" value="noen"/>
+                    <label>None</label>
+                    <input type="radio" name="addition" value="sitename"/>
+                    <label>Permalink</label>
+                    <input type="radio" name="addition" value="custom"/>
+                    <label>Custom</label>
+                </div>
+            </td>
+        </tr>
+        <tr class="row">
+            <td valign="top">
+                <label class="title">Custom link:</label>
+            </td>
+            <td valign="top">
+                <div class="form-wrapper llar">
+                <input type="text" name="custom_addition" placeholder="Browser title custom addition"/>
+                </div>
+            </td>
+        </tr>
+    
+    </tbody>
 </table>
