4a8df0643c3b95a202cc1e017f75f9518813cc10
Stephan <vanaf1979@gmail.com>
2019-02-10 20:51:30 +0100

    Added browser title functions

--- settings/partials/seobox-settings-google-display.php
+++ settings/partials/seobox-settings-google-display.php
@@ -4,20 +4,17 @@
     <thead>
         <tr>
             <td valign="top" width="170">
-                <h4>Browser title</h4>
+                <h4><?php _e( 'Browser title', 'seobox' ); ?></h4>
             </td>
             <td valign="top">
-                <!-- <div class="form-wrapper active">
-                    <input type="checkbox" name="" />
-                    <label>Show in post edit screens?</label>
-                </div> -->
+                &nbsp;
             </td>
         </tr>
     </thead>
     <tbody>
         <tr class="row">
             <td valign="top">
-                <label class="title">Active:</label>
+                <label class="title"><?php _e( 'Active', 'seobox' ); ?>:</label>
             </td>
             <td valign="top">
                 <div class="form-wrapper llar">
@@ -29,15 +26,15 @@
                     }
                     ?>
                     <input type="radio" id="_g_browser_title_active_yes" name="_g_browser_title_active" value="yes" <?php checked( $current_value, 'yes' ); ?>/>
-                    <label for="_g_browser_title_active_yes">Yes</label>
+                    <label for="_g_browser_title_active_yes"><?php _e( 'Yes', 'seobox' ); ?></label>
                     <input type="radio" id="_g_browser_title_active_no" name="_g_browser_title_active" value="no" <?php checked( $current_value, 'no' ); ?>/>
-                    <label for="_g_browser_title_active_no">No</label>
+                    <label for="_g_browser_title_active_no"><?php _e( 'No', 'seobox' ); ?></label>
                 </div>
             </td>
         </tr>
         <tr class="row">
             <td valign="top">
-                <label class="title">Addition:</label>
+                <label class="title"><?php _e( 'Addition', 'seobox' ); ?>:</label>
             </td>
             <td valign="top">
                 <div class="form-wrapper llar">
@@ -49,27 +46,27 @@
                     }
                     ?>
                     <input type="radio" id="_g_browser_title_addition_none" name="_g_browser_title_addition" value="none" <?php checked( $current_value, 'none' ); ?>/>
-                    <label for="_g_browser_title_addition_none">None</label>
+                    <label for="_g_browser_title_addition_none"><?php _e( 'None', 'seobox' ); ?></label>
                     <input type="radio" id="_g_browser_title_addition_sitename" name="_g_browser_title_addition" value="sitename" <?php checked( $current_value, 'sitename' ); ?>/>
-                    <label for="_g_browser_title_addition_sitename">Sitename</label>
+                    <label for="_g_browser_title_addition_sitename"><?php _e( 'Sitename', 'seobox' ); ?></label>
                     <input type="radio" id="_g_browser_title_addition_custom" name="_g_browser_title_addition" value="custom" <?php checked( $current_value, 'custom' ); ?>/>
-                    <label for="_g_browser_title_addition_custom">Custom</label>
+                    <label for="_g_browser_title_addition_custom"><?php _e( 'Custom', 'seobox' ); ?></label>
                 </div>
             </td>
         </tr>
         <tr class="row">
             <td valign="top">
-                <label class="title">Custom addition:</label>
+                <label class="title"><?php _e( 'Custom addition', 'seobox' ); ?>:</label>
             </td>
             <td valign="top">
                 <div class="form-wrapper llar">
-                <input type="text" name="_g_browser_title_custom_addition" placeholder="Browser title custom addition" value="<?php echo get_option('_g_browser_title_custom_addition'); ?>"/>
+                <input type="text" name="_g_browser_title_custom_addition" placeholder="<?php _e( 'Browser title custom addition', 'seobox' ); ?>" value="<?php echo get_option('_g_browser_title_custom_addition'); ?>"/>
                 </div>
             </td>
         </tr>
         <tr class="row">
             <td valign="top">
-                <label class="title">Position:</label>
+                <label class="title"><?php _e( 'Position', 'seobox' ); ?>:</label>
             </td>
             <td valign="top">
                 <div class="form-wrapper llar">
@@ -81,35 +78,35 @@
                     }
                     ?>
                     <input type="radio" id="_g_browser_title_addition_position_prefix" name="_g_browser_title_addition_position" value="prefix" <?php checked( $current_value, 'prefix' ); ?>/>
-                    <label for="_g_browser_title_addition_position_prefix">Prefix</label>
+                    <label for="_g_browser_title_addition_position_prefix"><?php _e( 'Prefix', 'seobox' ); ?></label>
                     <input type="radio" id="_g_browser_title_addition_position_suffix" name="_g_browser_title_addition_position" value="suffix" <?php checked( $current_value, 'suffix' ); ?>/>
-                    <label for="_g_browser_title_addition_position_suffix">Suffix</label>
+                    <label for="_g_browser_title_addition_position_suffix"><?php _e( 'Suffix', 'seobox' ); ?></label>
                 </div>
             </td>
         </tr>
         <tr class="row">
             <td valign="top">
-                <label class="title">Default value:</label>
+                <label class="title"><?php _e( 'Default value', 'seobox' ); ?>:</label>
             </td>
             <td valign="top">
                 <div class="form-wrapper llar">
-                    <input type="text" name="_g_browser_title_default" placeholder="Browser title default valeu" value="<?php echo get_option('_g_browser_title_default'); ?>"/>
+                    <input type="text" name="_g_browser_title_default" placeholder="<?php _e( 'Browser title default value', 'seobox' ); ?>" value="<?php echo get_option('_g_browser_title_default'); ?>"/>
                 </div>
             </td>
         </tr>
         <tr class="row">
             <td valign="top">
-                <label class="title">Max lenght:</label>
+                <label class="title"><?php _e( 'Max lenght', 'seobox' ); ?>:</label>
             </td>
             <td valign="top">
                 <div class="form-wrapper llar">
-                    <input type="text" name="_g_browser_title_max_lenght" placeholder="Browser title max lenght" value="<?php echo get_option('_g_browser_title_max_lenght'); ?>"/>
+                    <input type="text" name="_g_browser_title_max_lenght" placeholder="<?php _e( 'Browser title max length', 'seobox' ); ?>" value="<?php echo get_option('_g_browser_title_max_lenght'); ?>"/>
                 </div>
             </td>
         </tr>
         <tr class="row">
             <td valign="top">
-                <label class="title">Max length overflow:</label>
+                <label class="title"><?php _e( 'Max lenght overflow', 'seobox' ); ?>:</label>
             </td>
             <td valign="top">
                 <div class="form-wrapper llar">
@@ -121,9 +118,9 @@
                     }
                     ?>
                     <input type="radio" id="_g_browser_title_max_length_overflow_trim" name="_g_browser_title_max_length_overflow" value="trim" <?php checked( $current_value, 'trim' ); ?>/>
-                    <label for="_g_browser_title_max_length_overflow_trim">Trim</label>
+                    <label for="_g_browser_title_max_length_overflow_trim"><?php _e( 'Trim', 'seobox' ); ?></label>
                     <input type="radio" id="_g_browser_title_max_length_overflow_warn" name="_g_browser_title_max_length_overflow" value="warn" <?php checked( $current_value, 'warn' ); ?>/>
-                    <label for="_g_browser_title_max_length_overflow_warn">Warn</label>
+                    <label for="_g_browser_title_max_length_overflow_warn"><?php _e( 'Warn', 'seobox' ); ?></label>
                 </div>
             </td>
         </tr>
@@ -139,10 +136,7 @@
                 <h4>Keywords</h4>
             </td>
             <td valign="top">
-                <!-- <div class="form-wrapper active">
-                    <input type="checkbox" name="" />
-                    <label>Show in post edit screens?</label>
-                </div> -->
+                &nbsp;
             </td>
         </tr>
     </thead>
